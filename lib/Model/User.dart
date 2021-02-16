0dfc924bf6e6fc26add52068fb30f4fcfaa3fcc9
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2021-02-17 00:41:06 +0900

    Added User with ChangeNotifier

---
+++ lib/Model/User.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:wsu_course_helper/Model/SchedulePool.dart';
+
+class User with ChangeNotifier {
+
+  String _username;
+  SchedulePool _schedulePool;
+
+  User ({String userName, SchedulePool schedulePool}) {
+    _username = userName;
+    _schedulePool = schedulePool;
+  }
+
+  // provider methods
+  void setName (String username) {
+    _username = username;
+    notifyListeners();
+  }    
+}
